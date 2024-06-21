5367e09fceee1776a240d3a2b4d7f8b11c6d5dc7
berkterek <42406263+berkterek@users.noreply.github.com>
2024-06-21 23:32:08 +0300

    feat: movement visual side completed

---
+++ EnableComponentSample/Assets/_Scripts/Systems/SoldierVisualMoveSystem.cs
@@ -0,0 +1,27 @@
+using EnableComponents.Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace EnableComponents.Systems
+{
+    [BurstCompile]
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateBefore(typeof(TransformSystemGroup))]
+    [UpdateAfter(typeof(SoldierEntityMoveSystem))]
+    public partial class SoldierVisualMoveSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            foreach (var (soldierTag, moveTag, entity) in SystemAPI.Query<RefRO<SoldierTag>, RefRO<MoveTag>>().WithEntityAccess())
+            {
+                if (SystemAPI.ManagedAPI.HasComponent<SoldierVisualReferenceData>(entity))
+                {
+                    var referenceData = SystemAPI.ManagedAPI.GetComponent<SoldierVisualReferenceData>(entity);
+                    
+                    referenceData.Reference.IsWalking(true);
+                }
+            }
+        }
+    }
+}
